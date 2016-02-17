a77e1b8f9140d8f341361e3776289a66b9ae5103
Afton Downey and Brianna Popson <bpopson@gmail.com>
2016-02-17 09:39:37 -0800

    refactor playGame method to make it more readable

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -4,20 +4,18 @@ class RockPaperScissors
 {
       function playGame($first_input, $second_input)
       {
-          if ($first_input == "rock" && $second_input == "scissors"){
-              return "Player 1";
-          } elseif ($second_input == "rock" && $first_input == "scissors"){
-              return "Player 2";
-          } elseif ($first_input == "rock" && $second_input == "paper"){
-              return "Player 2";
-          } elseif ($second_input == "rock" && $first_input == "paper"){
+          $inputs = array('Player1' => $first_input, 'Player2' => $second_input);
+
+          if ($first_input == $second_input){
+              return "It's a Draw!";
+          } elseif ($first_input == "paper" && $second_input == "rock"){
               return "Player 1";
           } elseif ($first_input == "scissors" && $second_input == "paper"){
-                return "Player 1";
-          } elseif ($second_input == "scissors" && $first_input == "paper"){
-                return "Player 2";
-          } elseif ($first_input == $second_input){
-                return "It's a Draw!";
+              return "Player 1";
+          } elseif ($first_input == "rock" && $second_input == "scissors"){
+              return "Player 1";
+          } else {
+              return "Player 2";
           }
       }
 }
